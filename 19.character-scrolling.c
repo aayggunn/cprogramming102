1b8c8001ee89830919136a806d297c4c188cb9c1
Abdurrahman Aygun <139715066+aayggunn@users.noreply.github.com>
2025-03-13 15:43:48 +0300

    Add files via upload

---
+++ 19.character-scrolling.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void characterScroll(char* , int);
+
+int main() {
+	
+	char characters[8] = {'S', 'o', 'f', 't', 'w', 'a', 'r', 'e'};
+	int size = sizeof(characters) / sizeof(characters[0]);
+	characterScroll(characters, size);
+	
+	return 0;
+}
+void characterScroll(char* characters, int size){
+	int i=0, j=0;
+	for(i=0; i<size+1; i++){
+		for(j=i; j<size; j++){
+			printf("%c", * (characters+j));
+		}
+		for(j=0; j<i; j++){
+			printf("%c", * (characters+j));
+		}
+		printf("\n");
+	}
+}
